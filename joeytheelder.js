cf67aa6834336c8dee38cd699be5b9c918cc1e73
firebolthappy <>
2021-05-07 19:41:44 -0400

    Update joeytheelder.js

--- joeytheelder.js
+++ joeytheelder.js
@@ -5,5 +5,15 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.on('message', message => {
+  // If the message is '!rip'
+  if (message.content.includes("femboy")) {
+    // Create the attachment using MessageAttachment
+    const attachment = new MessageAttachment('https://i.redd.it/wvko7o6fnsv61.jpg'); //femboy kye
+    // Send the attachment in the message channel
+    message.channel.send(attachment);
+  }
+});
 
+//https://i.redd.it/wvko7o6fnsv61.jpg
 client.login(process.env.BOT_TOKEN);
