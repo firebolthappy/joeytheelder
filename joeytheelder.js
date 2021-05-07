a2efa048ed6740d3700e1255560c3eca8b07f21d
firebolthappy <>
2021-05-07 19:27:24 -0400

    Update joeytheelder.js

--- joeytheelder.js
+++ joeytheelder.js
@@ -3,7 +3,7 @@ const client = new Discord.Client(); // creates a discord client
 
 client.once('ready', () => {
 	console.log('Ready!');
-	client.user.setPresence({ game: { name: "Joe's House" }})
+	client.user.setPresence({ game: { name: "Joe's House" }});
 });
 
 
