6a762c5b2fa6677d66fb4e259d8f4ce2ab60f13c
firebolthappy <48652283+firebolthappy@users.noreply.github.com>
2021-05-07 17:03:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js"); // imports the discord library
+const fs = require("fs"); // imports the file io library
+
+const client = new Discord.Client(); // creates a discord client
+
+client.once("ready", () => { // prints "Ready!" to the console once the bot is online
+	console.log("Ready!");
+});
+
+client.login(BOT_TOKEN); // starts the bot up
