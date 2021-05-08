8f5cab7c6c2e910711aeb419b3040d676da26af1
firebolthappy <>
2021-05-07 21:03:28 -0400

    Update joeytheelder.js

--- joeytheelder.js
+++ joeytheelder.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js"); // imports the discord library
 const client = new Discord.Client(); // creates a discord client
 let msgin = message.content.includes
 let msgsnd = message.channel.send
-var number = getRandomInt(6);
+var num = 0;
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -11,18 +11,19 @@ client.once('ready', () => {
 client.on('message', message => {
   // If the message is '!rip'
 	if ((msgin("hello")) && (message.author.bot)) {
-		if number === 0 {
+		num = genRandomInt(6);
+		if num === 0 {
 			msgsnd("Hey!");
-		} else if number = 1 {
+		} else if num = 1 {
 			msgsnd("Hello!");
-		} else if number = 2 {
+		} else if num = 2 {
 			msgsnd("Hey, How's it going");
-		} else if number = 3 {
+		} else if num = 3 {
 			msgnd("Hello there");
-		} else if number = 4 {
+		} else if num = 4 {
 			msgnd("Howdy there");
-		} else if number = 5 {
-			msgnd("Aloha there buckaroo")
+		} else if num = 5 {
+			msgnd("Aloha there buckaroo");
 		}
 	} //end of hello
 
