0017ba4d59cb3f1ec0e5ca623f31829707987215
firebolthappy <>
2021-05-07 20:42:03 -0400

    Update joeytheelder.js

--- joeytheelder.js
+++ joeytheelder.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js"); // imports the discord library
 const client = new Discord.Client(); // creates a discord client
+let msgin = message.content.includes
+let msgsnd = message.channel.send
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -7,8 +9,9 @@ client.once('ready', () => {
 
 client.on('message', message => {
   // If the message is '!rip'
-  if (message.content.includes("femboy")) {
-    message.channel.send({file: 'https://i.redd.it/wvko7o6fnsv61.jpg'}); //femboy kye
+
+  if (msgin("femboy")) {
+    msgsnd({file: 'https://i.redd.it/wvko7o6fnsv61.jpg'}); //femboy kye
   }
 });
 
