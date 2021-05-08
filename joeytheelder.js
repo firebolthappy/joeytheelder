accf1b38dbdaed230337435a5fe4730fa22875c3
firebolthappy <>
2021-05-07 21:18:04 -0400

    Update joeytheelder.js

--- joeytheelder.js
+++ joeytheelder.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js"); // imports the discord library
 const client = new Discord.Client(); // creates a discord client
-var num = 0;
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -9,7 +8,7 @@ client.once('ready', () => {
 client.on('message', message => {
   // If the message is '!rip'
 	if ((message.content.includes("hello")) && !(message.author.bot)) {
-		num = genRandomInt(6);
+		var num = genRandomInt(6);
 		if (num === 0) {
 			message.content.send("Hey!");
 		} else if (num === 1) {
